b4c84bad66810de770d2b764ddc42e4e523c12b9
daniela <daniela.140872@gmail.com>
2024-06-10 18:51:09 -0300

    ADD: Code Refactor
    Co-Authored-By: biancardoso <113621020+biancardoso@users.noreply.github.com>

---
+++ lib/index.dart
@@ -0,0 +1,21 @@
+// Export pages
+export '/pages/registro_consulta/registro_consulta_widget.dart'
+    show RegistroConsultaWidget;
+export '/pages/detalhes_consulta/detalhes_consulta_widget.dart'
+    show DetalhesConsultaWidget;
+export '/pages/cadastro/cadastro_widget.dart' show CadastroWidget;
+export '/pages/tela_inicial/tela_inicial_widget.dart' show TelaInicialWidget;
+export '/pages/tela_acesso/tela_acesso_widget.dart' show TelaAcessoWidget;
+export '/pages/tela_login/tela_login_widget.dart' show TelaLoginWidget;
+export '/pages/tela_exames/tela_exames_widget.dart' show TelaExamesWidget;
+export '/pages/homepage/homepage_widget.dart' show HomepageWidget;
+export '/pages/medicamentos/medicamentos_widget.dart' show MedicamentosWidget;
+export '/pages/resultados_exames/resultados_exames_widget.dart'
+    show ResultadosExamesWidget;
+export '/pages/perfil/perfil_widget.dart' show PerfilWidget;
+export '/pages/historico_exames/historico_exames_widget.dart'
+    show HistoricoExamesWidget;
+export '/pages/detalhes_medicamentos/detalhes_medicamentos_widget.dart'
+    show DetalhesMedicamentosWidget;
+export '/pages/adicionar_medicamento/adicionar_medicamento_widget.dart'
+    show AdicionarMedicamentoWidget;
